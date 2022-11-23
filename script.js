15b7c24a05bd9e705da5ff3db7e6040f683c09cc
juliabergman <juliabergman523@gmail.com>
2022-11-23 18:34:45 -0300

    feat: animação js

---
+++ script.js
@@ -0,0 +1,35 @@
+ScrollReveal().reveal('.primeiro', {
+    delay: 350
+});
+
+ScrollReveal().reveal('.segundo', {
+    delay: 580
+});
+
+ScrollReveal().reveal('.terceiro', {
+    delay: 700
+});
+
+ScrollReveal().reveal('.quarto', {
+    delay: 450
+});
+
+ScrollReveal().reveal('.quinto', {
+    delay: 580
+});
+
+ScrollReveal().reveal('.sexto', {
+    delay: 880
+});
+
+ScrollReveal().reveal('.setimo', {
+    delay: 1180
+});
+
+ScrollReveal().reveal('.oitavo', {
+    delay: 580
+});
+
+ScrollReveal().reveal('.nono', {
+    delay: 780
+});
